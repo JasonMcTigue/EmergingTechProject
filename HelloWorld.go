5864b14251d24fb11367566a4f292739d331b539
GARY MC HUGH <G00308668@gmit.ie>
2016-10-17 15:43:25 +0100

    fixed template problems

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,7 +1,33 @@
+// Author: Ian McLoughlin
+// Date: October 6th, 2016
+// Adapted from: https://go-macaron.com
+
 package main
 
-import "fmt"
+import "gopkg.in/macaron.v1"
+
+// Function taken from: github.com/golang/example
+func reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+
+	m.Get("/macaron", func() string {
+		return "Hello from Macaron1!"
+	})
+
+	m.Get("/reverse/:name", func(ctx *macaron.Context) {
+		// Adapted from: https://go-macaron.com/docs/middlewares/templating
+		ctx.Data["Name"] = reverse(ctx.Params(":name"))
+		ctx.HTML(200, "hello")
+	})
+
+	m.Run(4001)
 }
